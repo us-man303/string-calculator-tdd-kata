2e9e9c0c4bbc82fb7876db1b10f55f8a758a3e69
us-man303 <usman52502@gmail.com>
2024-09-03 08:51:01 +0530

    Extend Add function to handle new line delimiters

--- calculator.py
+++ calculator.py
@@ -2,5 +2,6 @@ def add(numbers: str) -> int:
     if numbers == "":
         return 0
     else:
+        numbers = numbers.replace("\n", ",")
         num_list = map(int, numbers.split(','))
         return sum(num_list)
--- test_calculator.py
+++ test_calculator.py
@@ -3,3 +3,5 @@ from calculator import add
 assert add("") == 0
 assert add("1") == 1
 assert add("1,5") == 6
+assert add("1\n2,3") == 6
+
