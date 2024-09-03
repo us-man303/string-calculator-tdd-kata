9d7ad81d6d855eefeb0e19913a5c880640f27b7e
us-man303 <usman52502@gmail.com>
2024-09-03 08:57:43 +0530

    Add support for custom delimiters

--- calculator.py
+++ calculator.py
@@ -1,7 +1,16 @@
 def add(numbers: str) -> int:
     if numbers == "":
         return 0
+    
+    if numbers.startswith("//"):
+        delimiter_index = numbers.index("\n")
+        delimiter = numbers[2:delimiter_index]
+        numbers = numbers[delimiter_index + 1:]
+        num_list = map(int, numbers.split(delimiter))
     else:
         numbers = numbers.replace("\n", ",")
         num_list = map(int, numbers.split(','))
-        return sum(num_list)
+    
+    return sum(num_list)
+
+print(add("//;\n1;2"))
--- test_calculator.py
+++ test_calculator.py
@@ -4,4 +4,6 @@ assert add("") == 0
 assert add("1") == 1
 assert add("1,5") == 6
 assert add("1\n2,3") == 6
+assert add("//;\n1;2") == 3
+
 
